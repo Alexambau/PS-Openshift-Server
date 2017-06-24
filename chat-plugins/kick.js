34c5ca9e06fc3d134dac413a481ad60b196fbf8a
Zaiko584 <rubenjosexd9@gmail.com>
2017-06-23 21:14:35 -0400

    Update kick.js

--- chat-plugins/kick.js
+++ chat-plugins/kick.js
@@ -9,10 +9,10 @@ exports.commands = {
 			return this.sendReply("User " + this.targetUsername + " not found.");
 		}
 		if (!room.users[targetUser.userid]) {
-			return this.sendReply("User " + this.targetUsername + " is not in the room " + room.id + ".");
+			return this.sendReply("El usuario " + this.targetUsername + " no es de esta sala " + room.id + ".");
 		}
 		if (!this.can('kick', targetUser, room)) return false;
-		var msg = "kicked from room " + room.id + " by " + user.name + (target ? " (" + target + ")" : "") + ".";
+		var msg = "kickeado de la sala " + room.id + " por " + user.name + (target ? " (" + target + ")" : "") + ".";
 		this.addModCommand("" + targetUser.name + " was " + msg);
 		targetUser.popup("You have been " + msg);
 		targetUser.leaveRoom(room);
